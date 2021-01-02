dcefb6d2f2cfdbe2dd2266ce996c18e81fa46652
筱怪 <llqhz@qq.com>
2021-01-02 08:00:47 +0000

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,6 +9,8 @@
       </div>
 
       <div class="card-body">
+        @include('shared/_errors')
+
         <form action="{{ route('users.store') }}" method="post">
           {{ csrf_field() }}
           <div class="form-group">
@@ -23,12 +25,12 @@
 
           <div class="form-group">
             <label for="password">密码</label>
-            <input type="password" name="name" class="form-control" value="{{ old('password') }}" />
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}" />
           </div>
 
           <div class="form-group">
             <label for="password_confirmation">确认密码</label>
-            <input type="password" name="name" class="form-control" value="{{ old('password_confirmation') }}" />
+            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}" />
           </div>
 
           <button type="submit" class="btn btn-primary">注册</button>
